b7172619000e40bd4f5d75360f6be6ee256fd217
daoudacherif <160236296+daoudacherif@users.noreply.github.com>
2025-03-15 20:17:16 +0000

    Update dbconnection.php

--- admin/includes/dbconnection.php
+++ admin/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("Localhost", "root", "", "italalu");
+$con=mysqli_connect("Localhost", "root", "", "u553063725_italalu");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
