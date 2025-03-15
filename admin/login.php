6a2aa39b7418a1ac7145c90e4b148b9b7dd0727b
daoudacherif <160236296+daoudacherif@users.noreply.github.com>
2025-03-15 20:22:33 +0000

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,51 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
 
+if(isset($_POST['login']))
+  {
+    $adminuser=$_POST['username'];
+    $password=md5($_POST['password']);
+    $query=mysqli_query($con,"select ID from tbladmin where  UserName='$adminuser' && Password='$password' ");
+    $ret=mysqli_fetch_array($query);
+    if($ret>0){
+      $_SESSION['imsaid']=$ret['ID'];
+     header('location:dashboard.php');
+    }
+    else{
+    
+    echo '<script>alert("Invalid Details.")</script>';
+
+    }
+  }
+  if(isset($_POST['submit']))
+  {
+    $contactno=$_POST['contactno'];
+    $email=$_POST['email'];
+$password=md5($_POST['newpassword']);
+        $query=mysqli_query($con,"select ID from tbladmin where  Email='$email' and MobileNumber='$contactno' ");
+        
+    $ret=mysqli_num_rows($query);
+    if($ret>0){
+      $_SESSION['contactno']=$contactno;
+      $_SESSION['email']=$email;
+      $query1=mysqli_query($con,"update tbladmin set Password='$password'  where  Email='$email' && MobileNumber='$contactno'");
+       if($query1)
+   {
+echo "<script>alert('Password successfully changed');</script>";
+
+   }
+     
+    }
+    else{
+    
+      echo "<script>alert('Invalid Details. Please try again.');</script>";
+    }
+  }
+  ?>
+<!DOCTYPE html>
+<html lang="en">
     </head>
     <body>
         <div id="loginbox">            
