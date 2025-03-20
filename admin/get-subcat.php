6f37bc4fb0763665229e929438549b9b0f5b94f8
daoudacherif <daoudacherif4321@gmail.com>
2025-03-20 15:40:47 +0000

    GETSUB

--- admin/get-subcat.php
+++ admin/get-subcat.php
@@ -2,18 +2,39 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['imsaid']==0)) {
+
+// Vérifie que l'admin est connecté
+if (strlen($_SESSION['imsaid'] == 0)) {
   header('location:logout.php');
-  } else{
+  exit;
+}
 
- if(isset($_POST['catid'])){
- $cid=$_POST['catid'];
+if (isset($_POST['catid'])) {
+    $cid = intval($_POST['catid']);
 
-  $query=mysqli_query($con,"select * from tblsubcategory where CatID='$cid' && Status='1'");
-    while($rw=mysqli_fetch_array($query))
-    {
-     ?>      
- <option value="<?php echo $rw['ID'];?>"><?php echo $rw['SubCategoryname'];?></option>
-                  
+    // Requête pour récupérer les sous-catégories actives
+    $query = mysqli_query($con, "
+        SELECT ID, SubCategoryname
+        FROM tblsubcategory
+        WHERE CatID='$cid'
+          AND Status='1'
+        ORDER BY SubCategoryname ASC
+    ");
 
-<?php }}} ?>
+    $count = mysqli_num_rows($query);
+    if ($count > 0) {
+        // Afficher une option par défaut
+        echo '<option value="">Sélectionnez une sous-catégorie</option>';
+        // Boucle sur les résultats
+        while ($rw = mysqli_fetch_array($query)) {
+            echo '<option value="'.$rw['ID'].'">'.$rw['SubCategoryname'].'</option>';
+        }
+    } else {
+        // S'il n'y a pas de sous-catégorie pour cette catégorie
+        echo '<option value="">Aucune sous-catégorie disponible</option>';
+    }
+} else {
+    // Si catid n'est pas envoyé
+    echo '<option value="">Erreur: pas de catégorie reçue</option>';
+}
+?>
