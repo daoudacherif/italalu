17b7fe91ddd4e68cac2dd7af0fe06d3fbe90a6fe
daoudacherif <daoudacherif4321@gmail.com>
2025-03-23 13:13:11 +0000

    inventory update

--- admin/inventory.php
+++ admin/inventory.php
@@ -2,28 +2,30 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['imsaid']==0)) {
+if (strlen($_SESSION['imsaid'] == 0)) {
   header('location:logout.php');
-  } else{
-
-
-
-  ?>
+  exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<title>Système de Gestion d'Inventaire || Voir l'Inventaire des Produits</title>
-<?php include_once('includes/cs.php');?>
+  <title>Système de Gestion d'Inventaire || Voir l'Inventaire des Produits</title>
+  <?php include_once('includes/cs.php'); ?>
 </head>
 <body>
 
-<?php include_once('includes/header.php');?>
-<?php include_once('includes/sidebar.php');?>
-
+<?php include_once('includes/header.php'); ?>
+<?php include_once('includes/sidebar.php'); ?>
 
 <div id="content">
   <div id="content-header">
-    <div id="breadcrumb"> <a href="dashboard.php" title="Aller à l'accueil" class="tip-bottom"><i class="icon-home"></i> Accueil</a><strong> Voir l'Inventaire des Produits</strong> </div>
+    <div id="breadcrumb">
+      <a href="dashboard.php" title="Aller à l'accueil" class="tip-bottom">
+        <i class="icon-home"></i> Accueil
+      </a>
+      <strong>Voir l'Inventaire des Produits</strong>
+    </div>
     <h1>Voir l'Inventaire des Produits</h1>
   </div>
   <div class="container-fluid">
@@ -31,11 +33,9 @@ if (strlen($_SESSION['imsaid']==0)) {
     <div class="row-fluid">
       <div class="span12">
         
-       
-     
-        
         <div class="widget-box">
-          <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
+          <div class="widget-title">
+            <span class="icon"><i class="icon-th"></i></span>
             <h5>Inventaire des Produits</h5>
           </div>
           <div class="widget-content nopadding">
@@ -45,69 +45,118 @@ if (strlen($_SESSION['imsaid']==0)) {
                   <th>N°</th>
                   <th>Nom du Produit</th>
                   <th>Nom de la Catégorie</th>
-                   <th>Nom de la Sous-Catégorie</th>
+                  <th>Nom de la Sous-Catégorie</th>
                   <th>Nom de la Marque</th>
                   <th>Numéro de Modèle</th>
-                  <th>Stock</th>
+                  <!-- Retirer la colonne “Stock” -->
                   <th>Stock Restant</th>
                   <th>Statut</th>
+                  <th>Action</th> <!-- pour montrer si on peut ajouter au panier -->
                 </tr>
               </thead>
               <tbody>
                 <?php
-$ret=mysqli_query($con,"select tblcategory.CategoryName,tblsubcategory.SubCategoryname as subcat,tblproducts.ProductName,tblproducts.BrandName,tblproducts.ID as pid,tblproducts.Status,tblproducts.CreationDate,tblproducts.ModelNumber,tblproducts.Stock,sum(tblcart.ProductQty) as selledqty from tblproducts join tblcategory on tblcategory.ID=tblproducts.CatID join tblsubcategory on tblsubcategory.ID=tblproducts.SubcatID left join tblcart  on tblproducts.ID=tblcart.ProductId group by tblproducts.ProductName");
-$qty=$result['selledqty'];
-$num=mysqli_num_rows($ret);
-if($num>0){
-$cnt=1;
-while ($row=mysqli_fetch_array($ret)) {
-$qty=$row['selledqty'];
-?>
-                <tr class="gradeX">
-                  <td><?php echo $cnt;?></td>
-                  <td><?php  echo $row['ProductName'];?></td>
-                  <td><?php  echo $row['CategoryName'];?></td>
-                  <td><?php  echo $row['subcat'];?></td>
-                  <td><?php  echo $row['BrandName'];?></td>
-                  <td><?php  echo $row['ModelNumber'];?></td>
-                  <td><?php  echo $row['Stock'];?></td>
-                   <td><?php  echo ($_SESSION['rqty']=$row['Stock']-$qty);?></td>
-                  <?php if($row['Status']=="1"){ ?>
+                // On calcule le stock restant = tblproducts.Stock - sum(tblcart.ProductQty)
+                // On fait une LEFT JOIN sur tblcart pour connaître la quantité vendue
+                $ret = mysqli_query($con, "
+                  SELECT 
+                    tblcategory.CategoryName,
+                    tblsubcategory.SubCategoryname AS subcat,
+                    tblproducts.ProductName,
+                    tblproducts.BrandName,
+                    tblproducts.ID AS pid,
+                    tblproducts.Status,
+                    tblproducts.CreationDate,
+                    tblproducts.ModelNumber,
+                    tblproducts.Stock,
+                    SUM(tblcart.ProductQty) AS selledqty
+                  FROM tblproducts
+                  JOIN tblcategory ON tblcategory.ID = tblproducts.CatID
+                  JOIN tblsubcategory ON tblsubcategory.ID = tblproducts.SubcatID
+                  LEFT JOIN tblcart ON tblproducts.ID = tblcart.ProductId
+                  GROUP BY tblproducts.ID
+                  ORDER BY tblproducts.ID DESC
+                ");
 
-                     <td><?php echo "Actif"; ?></td>
-<?php } else { ?>                  <td><?php echo "Inactif"; ?>
-                  </td>
-                  <?php } ?>
-                                  
-                </tr>
-                <?php 
-$cnt=$cnt+1;
-} } else { ?>
-  <tr>
-    <td colspan="8"> Aucun enregistrement trouvé.</td>
+                $num = mysqli_num_rows($ret);
+                if ($num > 0) {
+                  $cnt = 1;
+                  while ($row = mysqli_fetch_array($ret)) {
+                    $qtySold = $row['selledqty'];
+                    if (!$qtySold) {
+                      $qtySold = 0; // si aucune vente
+                    }
+                    // Calcul du stock restant
+                    $stockRemain = $row['Stock'] - $qtySold;
+                    ?>
+                    <tr class="gradeX">
+                      <td><?php echo $cnt; ?></td>
+                      <td><?php echo $row['ProductName']; ?></td>
+                      <td><?php echo $row['CategoryName']; ?></td>
+                      <td><?php echo $row['subcat']; ?></td>
+                      <td><?php echo $row['BrandName']; ?></td>
+                      <td><?php echo $row['ModelNumber']; ?></td>
 
-  </tr>
-   
-<?php } ?>
+                      <!-- Stock Restant -->
+                      <td>
+                        <?php
+                        echo $stockRemain; 
+                        ?>
+                      </td>
+
+                      <!-- Statut (Actif/Inactif) -->
+                      <td>
+                        <?php 
+                        if ($row['Status'] == "1") {
+                          echo "Actif";
+                        } else {
+                          echo "Inactif";
+                        }
+                        ?>
+                      </td>
+
+                      <!-- Action : on empêche l’ajout au panier si stock <= 0 -->
+                      <td>
+                        <?php
+                        if ($stockRemain <= 0) {
+                          echo "<span style='color:red;'>Stock épuisé</span>";
+                        } else {
+                          // Ex. un lien ou bouton pour ajouter au panier
+                          echo "<a href='#' class='btn btn-mini btn-success'>Ajouter au panier</a>";
+                        }
+                        ?>
+                      </td>
+                    </tr>
+                    <?php
+                    $cnt++;
+                  }
+                } else {
+                  ?>
+                  <tr>
+                    <td colspan="9" style="text-align:center;">Aucun enregistrement trouvé.</td>
+                  </tr>
+                  <?php
+                }
+                ?>
               </tbody>
             </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!--Footer-part-->
-<?php include_once('includes/footer.php');?>
+          </div><!-- widget-content nopadding -->
+        </div><!-- widget-box -->
+      </div><!-- span12 -->
+    </div><!-- row-fluid -->
+  </div><!-- container-fluid -->
+</div><!-- content -->
+
+<?php include_once('includes/footer.php'); ?>
 <!--end-Footer-part-->
-<script src="js/jquery.min.js"></script> 
-<script src="js/jquery.ui.custom.js"></script> 
-<script src="js/bootstrap.min.js"></script> 
-<script src="js/jquery.uniform.js"></script> 
-<script src="js/select2.min.js"></script> 
-<script src="js/jquery.dataTables.min.js"></script> 
-<script src="js/matrix.js"></script> 
+
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.ui.custom.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.uniform.js"></script>
+<script src="js/select2.min.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/matrix.js"></script>
 <script src="js/matrix.tables.js"></script>
 </body>
 </html>
-<?php } ?>
