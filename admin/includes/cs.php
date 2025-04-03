46ce528d20a10797593f75c7f221cec3e7294b98
daoudacherif <daoudacherif4321@gmail.com>
2025-04-03 01:16:59 +0000

    removal css

--- admin/includes/cs.php
+++ admin/includes/cs.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="css/uniform.css" />
 <link rel="stylesheet" href="css/select2.css" />
 <link rel="stylesheet" href="css/fullcalendar.css" />
-<link rel="stylesheet" href="css/matrix-style.css" />
+<!-- <link rel="stylesheet" href="css/matrix-style.css" /> -->
 <link rel="stylesheet" href="css/matrix-media.css" />
 <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
 <link rel="stylesheet" href="css/jquery.gritter.css" />
