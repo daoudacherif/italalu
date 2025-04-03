721c9624ef08f74ff69a0a86ed278092ffa87829
daoudacherif <daoudacherif4321@gmail.com>
2025-04-03 02:24:42 +0000

    final today

--- admin/includes/cs.php
+++ admin/includes/cs.php
@@ -5,9 +5,7 @@
 <link rel="stylesheet" href="css/fullcalendar.css" />
 <link rel="stylesheet" href="css/matrix-style.css" />
 <link rel="stylesheet" href="css/matrix-media.css" />
-<!-- <link href="font-awesome/css/font-awesome.css" rel="stylesheet" /> -->
+<link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
 <link rel="stylesheet" href="css/jquery.gritter.css" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="css/font-awesome.min.css" />
-<!-- Ajoute ça dans ton fichier HTML dans <head> -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
